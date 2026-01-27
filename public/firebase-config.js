a1523c6a71e4d16004c587a9a10b7240182b959e
lehbunn-cloud <leh.bunn@gmail.com>
2026-01-27 15:52:39 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,16 +1,15 @@
 // ATENÇÃO: Substitua estas configurações pelas do SEU projeto Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyA1wB9c6fXJ7XvYqQqQqQqQqQqQqQqQqQqQ",
-    authDomain: "lehburn-cloud-fila-chamados-payhub.firebaseapp.com",
-    projectId: "lehburn-cloud-fila-chamados-payhub",
-    storageBucket: "lehburn-cloud-fila-chamados-payhub.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890abcdef",
-    measurementId: "G-ABCDEF1234"
+  apiKey: "AIzaSyCJXxaG8R5-VmkoGA7PyFcyfcvBAk92yTc",
+  authDomain: "portal-fila-payhub.firebaseapp.com",
+  projectId: "portal-fila-payhub",
+  storageBucket: "portal-fila-payhub.firebasestorage.app",
+  messagingSenderId: "28871537008",
+  appId: "1:28871537008:web:38d6ac22721f40a7d61fb5"
 };
 
 // Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Referências do Firebase
 const db = firebase.firestore();
@@ -22,3 +21,4 @@ window.firebaseApp = {
     auth: auth,
     firebase: firebase
 };
+
